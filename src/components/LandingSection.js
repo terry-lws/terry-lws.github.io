d5ffe42c93024dbb51a418475be843af4aadc011
terry-lws <terrylam299@gmail.com>
2023-04-04 15:38:06 +1000

    Change replace FullScreenSection with Box Set Box to flex and minHeight to 100vh Set GIF bg with gradient

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Heading, VStack, Text } from "@chakra-ui/react";
+import { Heading, VStack, Text, Box } from "@chakra-ui/react";
+import gif from "../images/codingv2.gif";
 import FullScreenSection from "./FullScreenSection";
 
 const hero = "Hi, I'm Terry";
@@ -7,12 +8,16 @@ const subhero = "Front-end Web Developer specialises in React";
 
 const LandingSection = () => {
   return (
-    <FullScreenSection
+    <Box
+    display="flex"
       px={8}
       py={20}
       justifyContent="center"
       alignItems="center"
-      backgroundColor="#dddddd"
+      minHeight="100vh"
+      color="white"
+      bg={`linear-gradient(to bottom, rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(${gif})`}
+      bgSize="cover"
       textAlign="center"
     >
       <VStack spacing={5} position="relative" bottom="10vh">
@@ -23,7 +28,7 @@ const LandingSection = () => {
           {subhero}
         </Text>
       </VStack>
-    </FullScreenSection>
+    </Box>
   )
 };
 
