1a36a86ea4eb9673a0f1a3556771cfcf7a4770a4
terry-lws <terrylam299@gmail.com>
2023-06-07 21:55:09 +1000

    Update projects object in ProjectSection.js
    Add webURL attribute to the object

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,32 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc, projectURL }) => {
+const Card = ({ title, description, imageSrc, projectURL, webURL }) => {
   return (
-    <Box
-      backgroundColor='white'
-      borderRadius='12px'
-      color='black'
-    >
-      <Image
-        src={imageSrc}
-        borderRadius='12px'>
-      </Image>
-      <VStack spacing={5}
-        p={[2, 4]}
-        align='left'>
-        <Heading size='md'>
-          {title}
-        </Heading>
-        <Text color='grey'>
-          {description}
-        </Text>
-        <Text fontWeight='600'
+    <Box backgroundColor="white" borderRadius="12px" color="black">
+      <Image src={imageSrc} borderRadius="12px"></Image>
+      <VStack spacing={5} p={[2, 4]} align="left">
+        <Heading size="md">{title}</Heading>
+        <Text color="grey">{description}</Text>
+        <Text
+          fontWeight="600"
           _hover={{
-            textDecoration: 'underline',
-          }}>
-          <a href={projectURL} target="_blank">GitHub repository <FontAwesomeIcon icon={faArrowRight} size='x1' /></a>
+            textDecoration: "underline",
+          }}
+        >
+          <a href={projectURL} target="_blank">
+            GitHub repository <FontAwesomeIcon icon={faArrowRight} size="x1" />
+          </a>
         </Text>
+        {webURL != "" && (
+          <Text
+            fontWeight="600"
+            _hover={{
+              textDecoration: "underline",
+            }}
+          >
+            <a href={webURL} target="_blank">
+              Website <FontAwesomeIcon icon={faArrowRight} size="x1" />
+            </a>
+          </Text>
+        )}
       </VStack>
     </Box>
   );
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -5,11 +5,20 @@ import Cards from "./Card";
 
 const projects = [
   {
-    title: "React Calculator App (on-going)",
+    title: "Claire Yau's personal portfolio (in development)",
+    description:
+      'A personal web portfolio for Claire Yau, a Psychology Graduate. By reading the portfolio, you will get to know more about Claire, her achievements, and professional career goal as a psychologist. This webpage is written in React, accompanied by the Chakra UI library.',
+    getImageSrc: () => require("../images/test.png"),
+    projectURL: "https://github.com/claireyau5/claireyau5.github.io",
+    webURL: "https://claireyau5.github.io/",
+  },
+  {
+    title: "React Calculator App (in development)",
     description:
       "A simple calculator app with basic calculation functions constructed with React Hooks such as useState and useRef. The calculator has the four basic operators, i.e., addition, deduction, multiplication, and division, as well as a clear input and a clear all functions.",
     getImageSrc: () => require("../images/calculator.PNG"),
-    projectURL: "https://github.com/terry-lws/weather-app",
+    projectURL: "https://github.com/terry-lws/calculator",
+    webURL: "",
   },
   {
     title: "React Weather App",
@@ -17,6 +26,7 @@ const projects = [
       "My first React project. A simple weather report web app constructed mainly with React and CSS. The app fetches real-time data of a location such as temperature, weather condition and wind speed from a third party online service using API.",
     getImageSrc: () => require("../images/weather.PNG"),
     projectURL: "https://github.com/terry-lws/weather-app",
+    webURL: "",
   },
   {
     title: "Predicting Diabetes Using Machine Learning Methods",
@@ -24,13 +34,7 @@ const projects = [
       "Written in Python. A coursework project that applied machine learning methods such as SVM and KNN, to predict potential diabetes diseases in patients using the Pima Indians Diabetes Database. Outcomes and findings of the experiments are documented in a research paper.",
     getImageSrc: () => require("../images/prml.png"),
     projectURL: "https://github.com/terry-lws/Using-Machine-Learning-to-Predict-Diabetes.git",
-  },
-  {
-    title: "Computer Vision - Bird Breeds Classification",
-    description:
-      "Written in MATLAB accompanied by the Deep Learning ToolBox. A coursework project that experimented with machine learning approaches such as SVM, HOG, and deep learning (CNN) to observe the classification performance of different ML approaches using the Caltech-UCSD Birds-200-2011 dataset.",
-    getImageSrc: () => require("../images/bird_classification.png"),
-    projectURL: "https://github.com/terry-lws/Bird-Breeds-Classification",
+    webURL: "",
   },
 ];
 
@@ -45,9 +49,7 @@ const ProjectsSection = () => {
       alignItems="flex-start"
       spacing={8}
     >
-      <Heading as="h1">
-        Projects
-      </Heading>
+      <Heading as="h1">Projects</Heading>
       <Box
         display="grid"
         gridTemplateColumns={{ lg: "repeat(2,minmax(0,1fr))", md: "repeat(1,minmax(0,1fr))" }}
@@ -60,6 +62,7 @@ const ProjectsSection = () => {
             description={project.description}
             imageSrc={project.getImageSrc()}
             projectURL={project.projectURL}
+            webURL={project.webURL}
           />
         ))}
       </Box>
