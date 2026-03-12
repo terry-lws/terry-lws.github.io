7580e03e61d3f2a7cf953dab8a39ac63e8b4ac03
terry-lws <terrylam299@gmail.com>
2026-03-12 23:40:48 +1100

    Update and replace projects with relevant web projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -9,23 +9,12 @@ import {
   Text,
   Divider,
   Link,
+  Tag,
 } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 const projects = [
-  //   {
-  //     title: "Claire Yau's personal portfolio (in development)",
-  //     description:
-  //       "A personal web portfolio for Claire Yau, a Psychology Graduate. By reading the portfolio, you will get to know more about Claire, her achievements, and professional career goal as a psychologist. This webpage is written in React, accompanied by the Chakra UI library.",
-  //     getImageSrc: () => require("../images/test.png"),
-  //     projectURL: "https://github.com/claireyau5/claireyau5.github.io",
-  //   },
-  //   {
-  //     title: "React Calculator App",
-  //     description:
-  //       "A simple calculator app with basic calculation functions constructed with React Hooks such as useState and useRef. The calculator has the four basic operators, i.e., addition, deduction, multiplication, and division, as well as a clear input and a clear all functions.",
-  //     getImageSrc: () => require("../images/calculator.PNG"),
-  //     projectURL: "https://github.com/terry-lws/calculator",
-  //   },
   {
     title: "LuggyShare",
     description:
@@ -33,24 +22,28 @@ const projects = [
     getImageSrc: () => require("../images/ls.PNG"),
   },
   {
-    title: "Australian Council of Education Research OS Test",
+    title: "React UI Implementation",
     description:
-      "This project is a web development assessment/test provided by Australian Council of Education Research (ACER). This project is written in JavaScript (React). Instructions are provided to install and run the website.",
+      "Implemented a responsive marketing landing page based on a Figma design. The project focuses on translating a UI design into a functional web interface using React components and modern CSS layout techniques.",
     getImageSrc: () => require("../images/acer.PNG"),
-    github: "https://github.com/terry-lws/os-test",
+    stack: ["React", "JavaScript", "Chakra UI", "CSS", "HTML"],
+    github: "https://github.com/terry-lws/react-ui-implementation",
+    demo: "https://react-ui-implementation-demo.vercel.app",
   },
   {
     title: "Weather App",
     description:
       "My first React project. A simple weather report web app constructed with React and JavaScript. The app fetches real-time data of a location such as temperature, weather condition and wind speed from a third party online service using API.",
     getImageSrc: () => require("../images/weather.PNG"),
-    github: "https://github.com/terry-lws/weather-app"
+    github: "https://github.com/terry-lws/weather-app",
   },
   {
-    title: "Predicting Diabetes Using Machine Learning Methods",
+    title: "Freelance WordPress Business Website",
     description:
       "Written in Python. A project that applies machine learning methods such as support vector machine and k-nearest neighbors algorithm, to predict potential diabetes diseases in patients using the Pima Indians Diabetes Database. Outcomes and findings are documented in a research paper.",
-    getImageSrc: () => require("../images/prml.png"),
+    getImageSrc: () => require("../images/Speedmedia.PNG"),
+    stack: ["WordPress", "Kubio", "CSS", "HTML"],
+    site: "https://speedmedia.com.hk/",
   },
 ];
 
@@ -88,11 +81,37 @@ const ProjectsSection = () => {
                   {project.title}
                 </Heading>
                 <Text>{project.description}</Text>
+                <HStack>
+                  {project.stack &&
+                    project.stack.map((st) => (
+                      <Tag key={st} variant="solid" colorScheme="teal">
+                        {st}
+                      </Tag>
+                    ))}
+                </HStack>
                 {project.github && (
                   <Link fontWeight={600} href={project.github} target="_blank">
-                    Github Repository
+                    <Text>
+                      {" "}
+                      Github Repository{" "}
+                      <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    </Text>
                   </Link>
                 )}
+                <Link fontWeight={600} href={project.demo} target="_blank">
+                  {project.demo && (
+                    <Text>
+                      View demo <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    </Text>
+                  )}
+                </Link>
+                <Link fontWeight={600} href={project.site} target="_blank">
+                  {project.site && (
+                    <Text>
+                      View website <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    </Text>
+                  )}
+                </Link>
               </VStack>
             </HStack>
             <Divider borderColor={"black"} />
